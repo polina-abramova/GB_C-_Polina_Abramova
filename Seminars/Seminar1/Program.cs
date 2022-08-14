44cc5cc33f66b4bf30030b686573dd6d09e37e42
--Polina <pskrevik@gmail.com>
2022-08-14 12:26:34 +0300

    seminar 2 added

---
+++ Seminars/Seminar1/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+//Задача 1. Возвести число в квадрат
+Console.Write("Input a number ");
+int num = Convert.ToInt32(Console.ReadLine ());
+
+int quad = num * num;
+
+//Console.WriteLine ("Your square of " + num + " is " + quad);
+Console.WriteLine ($"Your square of {num} is {quad}");
+*/
+
+/*
+//Задача 2. Проверить, является ли число 1 кваратом числа 2
+
+Console.Write("Input first number ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = n2 * n2;
+if (n1 == quad2)
+    Console.WriteLine ("n1 is quad of n2");
+else
+    Console.WriteLine ("n1 is NOT quad of n2");
+*/
+
+//Задача 3. Принимает на вход одно число, а на выходе показывает все целые числа от отрицательной версии до положительной
+
+/*
+Console.WriteLine("Input positive number");
+int number = Convert.ToInt32(Console.ReadLine());
+int current_number = number * (-1);
+
+while (current_number <= number)
+{
+    Console.Write(current_number +" ");
+    current_number++;
+}
+*/
+
+// Задача 4. Принимает трехзначное число, на выходе - последняя цифра этого числа
+
+Console.WriteLine("Input integer threedigit number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int cut_number = number %10 ;
+
+Console.Write ($"The last symbol of {number} is {cut_number}");
---
+++ Seminars/Seminar2/Program.cs
@@ -0,0 +1,84 @@
+﻿// Task 1. Задача, которая выводит случайное число из отрезка [10,99] и показывает его наибольшую цифру
+/*
+int FindBiggestDigit(int number)
+{
+    int ed = number % 10; //цифра из разряда единиц
+    int dec = number / 10; //цифра из десятки
+    int max;
+
+    if (ed>dec)
+        max = ed;
+    
+    else
+        max = dec;
+
+    return max; 
+}
+
+//Console.Write ("Random two-Digit number is" );
+
+int randomNumber = new Random().Next(10,100);
+
+int biggestDigit = FindBiggestDigit(randomNumber);
+
+Console.WriteLine($"Biggest digit of {randomNumber} is {biggestDigit}");
+
+*/
+
+/*
+//Задача 2.Программа выводит случайное 3-х значное число и удаляет вторую цифру
+int Cutnumber (int number)
+{
+    int ed = number %10;
+    int sot = number/100;
+
+    int result = sot *10 + ed;
+    return result;
+}
+
+int randomNumber = new Random().Next (100,1000);
+
+int NewNumber = Cutnumber (randomNumber);
+Console.WriteLine ($"New version of {randomNumber} is {NewNumber}");
+*/
+
+//Задача 3. Принимает на вход 2 числа и выводит, является ли число кратным первому. Если нет - остаток от деления
+/*
+void IsDivisionable (int num1, int num2)
+{
+    int reminder = num2 % num1;
+    
+    if (reminder == 0)
+    {
+        Console.WriteLine ($"{num2} is divisionable of {num1}");
+    }
+
+    else 
+    {
+        Console.WriteLine ($"{num2} is not divisionable of {num1}. Reminder is {reminder}");
+    }
+}
+
+Console.Write ("Imput first number ");
+int n1 = Convert.ToInt32(Console.ReadLine ());
+Console.Write ("Imput second number ");
+int n2 = Convert.ToInt32(Console.ReadLine ());
+
+IsDivisionable (n1,n2);
+
+*/
+
+//Задача 4. Принимает число, проверяет, кратно ли оно 7 И 23
+bool IsDivis(int number)
+{
+    if (number % 7==0 && number % 23 == 0)
+    return true ;
+    else
+    return false;
+}
+
+Console.Write ("Imput number ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+bool IsDivision = IsDivis(n);
+Console.WriteLine (IsDivision);
