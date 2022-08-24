c00eba302fb54c1e649a0e8f8fa85fcbc645d595
--Polina <pskrevik@gmail.com>
2022-08-24 23:06:43 +0300

    homework 6 renewed

--- HomeWorks/HomeWork6/Program.cs
+++ HomeWorks/HomeWork6/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-/*
+
 int CountPositive (int [] arr)
 {
     int count = 0;
@@ -13,17 +13,17 @@ int CountPositive (int [] arr)
     return count;
 }
 
-Console.Write ("Введите любое количество чисел через пробел: ");
+Console.Write ("Введите любое количество целых чисел через пробел: ");
 
 int[] array_from_string = Array.ConvertAll(Console.ReadLine().Split(), int.Parse); //преобразуем стринговый массив в массив чисел
 
 int result = CountPositive (array_from_string);
 
 Console.WriteLine ($"Вы ввели {result} положительных чис-ел (-а)!");
-*/
+
 
 //Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-
+/*
 void FindCrossPoint (double k1, double b1, double k2, double b2)
 {
     double x = -(b1 - b2) / (k1 - k2);
@@ -43,3 +43,4 @@ Console.Write("Задайте b2: ");
 double b2 = Convert.ToDouble(Console.ReadLine());
 
 FindCrossPoint (k1, b1, k2, b2);
+*/
